1c75206a20364bfb301a9648d050ac4f226608cb
Katie Gaskin <katie@MacBook-Pro.local>
2020-11-07 11:01:41 -0500

    popups were created and positioned

---
+++ Independent/pic.js
@@ -0,0 +1,29 @@
+function alert1()
+{
+    alert(1);
+}
+function showPopup(textContent, leftPosition, topPosition) {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    popup.textContent = textContent;
+    popup.style.left = leftPosition;
+    popup.style.top = topPosition;
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
