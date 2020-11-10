d3412f15374152ae31e5d9a3136a14727c1ec7d3
Katie Gaskin <katie@MacBook-Pro.local>
2020-11-09 19:39:11 -0500

    VIDEOS!!!!!

--- Independent/pic.js
+++ Independent/pic.js
@@ -9,3 +9,26 @@ function showPopup(textContent, leftPosition, topPosition) {
     popup.style.left = leftPosition;
     popup.style.top = topPosition;
 }
+
+function playVid() { 
+    var vid = document.getElementById("backgroundvid"); 
+    vid.play(); 
+  } 
+  
+  function pauseVid() { 
+    var vid = document.getElementById("backgroundvid"); 
+    vid.pause(); 
+  } 
+function showHideback() {
+    document.getElementById("backgroundvid").style.visibility="visible";
+}
+function showHide2() {
+    document.getElementById("straw").style.visibility="hidden";
+}
+function showHide3() {
+    document.getElementById("straw").style.visibility="visible";
+}
+function showHideleaf() {
+    document.getElementById("backgroundvid").style.visibility="visible";
+}
+
